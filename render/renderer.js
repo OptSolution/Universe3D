69f797b2678864a16f1eb5fc14422a6297a3cc22
OptSolution <mr_cwang@foxmail.com>
2020-04-28 00:12:45 +0800

    fix control

--- render/renderer.js
+++ render/renderer.js
@@ -2,6 +2,7 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 var THREE = require('three');
+var { OrbitControls } = require('three/examples/js/controls/OrbitControls');
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -12,17 +13,21 @@ var renderer = new THREE.WebGLRenderer( { canvas: canvas, context: context } );
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+
 var geometry = new THREE.BoxGeometry();
 var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
+controls.update();
 
 function animate() {
     requestAnimationFrame(animate);
     cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+    cube.rotation.y += 0.01;
+    controls.update();
     renderer.render(scene, camera);
 }
 animate();
