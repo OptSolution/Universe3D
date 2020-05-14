046dc5d7d74104371141a969235b06019da57a48
Chen Wang <mr_cwang@foxmail.com>
2020-05-14 22:35:08 +0800

    add grid and axis

--- src/u3d/ui/u3dUI.ts
+++ src/u3d/ui/u3dUI.ts
@@ -4,15 +4,34 @@
  * @Email: mr_cwang@foxmail.com
  * @Date: 2020-05-14 20:55:40
  * @LastEditors: Chen Wang
- * @LastEditTime: 2020-05-14 21:41:47
+ * @LastEditTime: 2020-05-14 22:31:20
  */
 import dat = require('dat.gui');
 import THREE = require('three');
 
 class U3dSceneMenu {
   background: string;
+  // axis
+  showAxis: boolean;
+  axisLength: number;
+  //grid
+  showGrid: boolean;
+  gridSize: number;
+  gridDivisions: number;
+  gridColorCenterLine: string;
+  gridColorGrid: string;
+
   constructor() {
     this.background = '#f0f0f0';
+
+    this.showAxis = false;
+    this.axisLength = 0.3;
+
+    this.showGrid = false;
+    this.gridSize = 1;
+    this.gridDivisions = 10;
+    this.gridColorCenterLine = '#444444';
+    this.gridColorGrid = '#888888';
   }
 }
 
@@ -38,5 +57,28 @@ export class U3dUI {
       colorValue = parseInt(colorValue.replace('#', '0x'), 16);
       scene.background = new THREE.Color(colorValue);
     });
+
+    // axis
+    let axesHelper = new THREE.AxesHelper(this.sceneMenu.axisLength);
+    axesHelper.visible = this.sceneMenu.showAxis;
+    scene.add(axesHelper);
+    let axisFolder = this.sceneFolder.addFolder('Axis');
+    axisFolder.add(this.sceneMenu, 'showAxis').onChange((show) => {
+      axesHelper.visible = show;
+    })
+    axisFolder.add(this.sceneMenu, 'axisLength').onChange((length) => {
+      axesHelper.scale.x = length;
+      axesHelper.scale.y = length;
+      axesHelper.scale.z = length;
+    })
+
+    // grid
+    let gridHelper = new THREE.GridHelper(this.sceneMenu.gridSize, this.sceneMenu.gridDivisions);
+    gridHelper.visible = this.sceneMenu.showGrid;
+    scene.add(gridHelper);
+    let gridFolder = this.sceneFolder.addFolder('Grid');
+    gridFolder.add(this.sceneMenu, 'showGrid').onChange((show) => {
+      gridHelper.visible = show;
+    })
   }
 }
